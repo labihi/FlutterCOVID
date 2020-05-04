77576f2b2198a489ebb2e5e7321e22b846ebeba2
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-05 00:16:33 +0200

    base UI

---
+++ lib/mainpage/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+import 'mainDrawer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: mainSwatch,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("WRYYYY"),
+            actions: <Widget>[
+              Icon(Icons.search)
+            ],
+          ),
+          drawer: Drawer(
+            child:  MainPageDrawer(),
+          ),
+
+        )
+    );
+  }
+}
