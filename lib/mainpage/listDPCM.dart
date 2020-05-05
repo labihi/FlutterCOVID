86a4a594c0cf4af219987d571b83fb7877c3e59a
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-05 12:30:23 +0200

    json parse -> listview working

--- lib/mainpage/listDPCM.dart
+++ lib/mainpage/listDPCM.dart
@@ -1,22 +1,17 @@
 import 'dart:async';
-//JSON?
 import 'dart:convert';
-import 'dart:developer';
-import 'dart:io';
 
-//HTTP requests
 import 'package:http/http.dart' as http;
-
-
 import 'package:flutter/material.dart';
 
 class ListDPCM extends StatefulWidget{
   @override
-  _ListDPCMState createState() => _ListDPCMState();
+  State<StatefulWidget> createState() => ListDCPMState();
+
 }
 
-class _ListDPCMState extends State<ListDPCM> {
-  List<ListTile> dpcmTiles = [];
+class ListDCPMState extends State<ListDPCM>{
+  List<dynamic> dpcmList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -24,33 +19,31 @@ class _ListDPCMState extends State<ListDPCM> {
     loadDPCMs();
     //creating an empty listView that will be populated with DPCMs later
     return Scrollbar(
-      child: ListView(
-        children: dpcmTiles,
+      child: ListView.separated(
+          itemCount: dpcmList.length,
+          itemBuilder: buildWidgetFromDPCM,
+        separatorBuilder: (context, index) =>  Divider(),
       ),
     );
   }
 
   Future<void> loadDPCMs() async {
+    //TODO: scopri perché ci sono &nbsp; nelle stringhe (probabile difetto del crawler)
     //download json
-    log("DOWNLOAD START");
-
-    //download and wait for download to be finished
     String response = (await http.get('https://federicocapece.dev/api/dpcm')).body;
-
     //parse json
-    Iterable dpcms = json.decode(response);
-    dpcms.forEach(addDPCMTile);
+    Iterable dpcmIterable = json.decode(response);
+    //add dpcms to the list
+    dpcmIterable.forEach((dpcm) => dpcmList.add(dpcm));
   }
 
-  void addDPCMTile(dynamic dpcmData){
-    log(dpcmData);
-    dpcmTiles.add(
-        ListTile(
-          title: Text("Title"),
-          subtitle: Text("subtitle"),
-          //onTap: ,
-        )
+  //this function build a ListTile from the index of a dpcm
+  Widget buildWidgetFromDPCM(BuildContext context, int index){
+    print("BUILDING: " + dpcmList[index].toString());
+    return ListTile(
+      title: Text(dpcmList[index]['title']),
+      subtitle: Text(dpcmList[index]['description']),
     );
-
   }
+
 }
