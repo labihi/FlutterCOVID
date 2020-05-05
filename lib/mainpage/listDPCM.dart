0fbf12be3803e0462276f3a9c41bba959f952ef9
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-05 11:00:03 +0200

    using dart http again

--- lib/mainpage/listDPCM.dart
+++ lib/mainpage/listDPCM.dart
@@ -1,17 +1,28 @@
 import 'dart:async';
+//JSON?
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
+//HTTP requests
+import 'package:http/http.dart' as http;
+
+
 import 'package:flutter/material.dart';
 
-class ListDPCM extends StatelessWidget{
+class ListDPCM extends StatefulWidget{
+  @override
+  _ListDPCMState createState() => _ListDPCMState();
+}
+
+class _ListDPCMState extends State<ListDPCM> {
   List<ListTile> dpcmTiles = [];
 
   @override
   Widget build(BuildContext context) {
-    log("DIOCANE");
-    loadDPCM();
+    //starting asynchronous function to load DPCMs
+    loadDPCMs();
+    //creating an empty listView that will be populated with DPCMs later
     return Scrollbar(
       child: ListView(
         children: dpcmTiles,
@@ -19,56 +30,27 @@ class ListDPCM extends StatelessWidget{
     );
   }
 
-  Future<void> loadDPCM() async {
-    log("LOADING DPCM");
-
+  Future<void> loadDPCMs() async {
     //download json
     log("DOWNLOAD START");
 
-    HttpClient client = new HttpClient();
-    client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
-    var request = await client.getUrl(Uri.parse('https://federicocapece.dev/api/dpcm'));
-    var response = await request.close();
+    //download and wait for download to be finished
+    String response = (await http.get('https://federicocapece.dev/api/dpcm')).body;
 
-    log(response.headers.toString());
-    log("LEN:"+response.contentLength.toString());
-
-    await for (var contents in response.transform(Utf8Decoder())) {
-      log("A"+contents);
-    }
-
-    log(response.statusCode.toString());
-    log("DOWNLOADED");
-    client.close(force: true);
-
-    /*
-    http.get("https://federicocapece.dev/api/dpcm").then(
-        (response){
-          log("HTTPS GOT RESULT");
-          Iterable dpcms = json.decode(response.body);
-          dpcms.forEach(
-            (dpcm) {
-              log(dpcm);
-            }
-          );
-        }
-    );*/
-
-    //parse it
-
-    //add the ListTiles to dpcm
-
-    //KEBAB
-    for(var i=0; i<10; i++){
-      dpcmTiles.add(
-          ListTile(
-            title: Text("Title"),
-            subtitle: Text("subtitle"),
-            //onTap: ,
-          )
-      );
-    }
-
+    //parse json
+    Iterable dpcms = json.decode(response);
+    dpcms.forEach(addDPCMTile);
   }
 
+  void addDPCMTile(dynamic dpcmData){
+    log(dpcmData);
+    dpcmTiles.add(
+        ListTile(
+          title: Text("Title"),
+          subtitle: Text("subtitle"),
+          //onTap: ,
+        )
+    );
+
+  }
 }
