d0c605833af4db96e28fc5c0da29e246dab574e2
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-05 12:29:55 +0200

    removed unused stuff

--- lib/mainpage/mainDrawer.dart
+++ lib/mainpage/mainDrawer.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:fluttertestapp/main.dart';
-
 import '../colors.dart';
 
 class MainPageDrawer extends StatelessWidget{
