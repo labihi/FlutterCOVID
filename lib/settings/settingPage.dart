9180e8010df7194e646307f32f01ae0b3c6c6223
labihi <mekhipipher@gmail.com>
2020-05-07 01:13:29 +0200

    Settings page bug

--- lib/settings/settingPage.dart
+++ lib/settings/settingPage.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget{
+class SettingsPage extends StatefulWidget{
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  List<String> regionList = ["Abruzzo", "Basilicata", "Calabria", "Campania", "Emilia-Romagna", "Friuli-Venezia Giulia", "Lazio", "Liguria", "Lombardia", "Marche", "Molise", "Piemonte", "Puglia", "Sardegna", "Sicilia", "Toscana", "Trentino-Alto Adige", "Umbria", "Valle d'Aosta", "Veneto"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,9 +19,21 @@ class SettingsPage extends StatelessWidget{
             ListTile(
               leading: Icon(Icons.map),
               title: Text('Regioni'),
+
+              ),
+            ListView.separated(
+              itemCount: regionList.length,
+              itemBuilder: widgetBuilder,
+              separatorBuilder: (context, index) =>  Divider(),
             ),
           ]
       ),
     );
+  }
+
+  Widget widgetBuilder(BuildContext context, int index){
+      return ListTile (
+        title: Text('BESTIA'),
+      );
   }
 }
