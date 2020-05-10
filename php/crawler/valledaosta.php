5fa1aa66cb11616f2bf662ae974e0a0d18795d56
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-10 15:54:36 +0200

    valle di merda

--- php/crawler/valledaosta.php
+++ php/crawler/valledaosta.php
@@ -3,43 +3,49 @@
 include_once("simple_html_dom.php");
 
 class Valledaosta{
-    
+
     static function parse($debug = false){
+        // scarico il file e trovo i tbody
         $html = file_get_html('https://www.regione.vda.it/pressevda/Eventi/decreti_ordinanze_i.aspx');
         $elements = $html->find('tbody',0)->children();
+
         for($i=0; $i < count($elements); $i++){
+            //ottengo testo e url
             $text = $elements[$i]->getElementsByTagName ("td",1)->plaintext;
             $url    = $elements[$i]->getElementsByTagName ("td",2)->children(0)->href;
-            //var_dump ($url);
-            //echo $url."<br>";
-            preg_match("/[^0-9][0-9]{1,2} \w* [0-9]{2,4}/",$text, $res);
-            $posData = strpos($text,$res[0]);
-            if($posData<(strlen($text)/2)){
-                $title = trim(substr($text,0,$posData));
-            }else{
-                if($pos = Valledaosta::findFirstBadCharacter($text)){
-                    $title = trim(substr($text,0,$pos));
-                }else{
-                    $title = trim(substr($text,0,$posData));
-                }
-                
-            }
-            var_dump($pos);
-            echo "<br>";
-            echo $title;
-            echo "<br>";
-            echo "<br>";
+            echo "G: ". $text . "<br>";
             
+            //cerco le date
+            preg_match("/[^0-9][0-9]{1,2} \w* [0-9]{2,4}/",$text, $res);
+
+            //cerco la data maggiore
+            $maxDate = Valledaosta::findMaxDate($res);
+
+            tryUpload($url,$maxDate,$text,'valle',[]);
         }
     }
-    static function findFirstBadCharacter($text){
-        for ($i=0; $i < strlen($text); $i++) { 
-            $char = $text[$i];
-            if(!(is_numeric($char)||ctype_alpha($char)||$char == " "||$char == "°"||$char == ".")){
-                return $i;
+
+    static function findMaxDate($dates){
+        global $mesi;
+
+        $mostRecent= 0;
+        $mostRecentOriginal = "";
+        foreach($dates as $date){
+            $originalDate = substr($date,1);
+            //pulisco la data, la esplodo e traduco il mese
+            $date = explode(' ', $originalDate);
+            $date[1] = $mesi[$date[1]];
+            $date = join('-', array_reverse($date));
+
+            $date = strtotime($date);
+            if ($date > $mostRecent) {
+                $mostRecent = $date;
+                $mostRecentOriginal = $originalDate;
             }
         }
-        return false;
+
+        return $mostRecentOriginal;
     }
+
 }
 ?>
